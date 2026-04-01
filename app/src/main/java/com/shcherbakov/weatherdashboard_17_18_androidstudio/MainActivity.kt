fa77109ea1e7538b5e81693d0b6092d89cc198a1
whypikmi <ttelatnikova1@yandex.ru>
2026-04-01 09:55:25 +0300

    Initial commit

---
+++ app/src/main/java/com/shcherbakov/weatherdashboard_17_18_androidstudio/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.shcherbakov.weatherdashboard_17_18_androidstudio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shcherbakov.weatherdashboard_17_18_androidstudio.ui.theme.WeatherDashboard_1718_androidstudioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherDashboard_1718_androidstudioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherDashboard_1718_androidstudioTheme {
+        Greeting("Android")
+    }
+}
